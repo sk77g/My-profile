343fe213578cc3a7ab414425a4d171ebb9f1de96
sk77g <76616882+sk77g@users.noreply.github.com>
2023-03-30 00:55:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,122 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+/*-------contact form----*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactProject = document.getElementById('contact-project'),
+   contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //check if the field has a value
+
+    if(contactName.value === ' ' || contactEmail.value === ' ' || contactProject.value === ' '){
+        //add or remove color
+
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+
+        //show message
+        contactMessage.textContent = 'write all the input fields';
+    }
+    else{
+        //serviceId - templetId - #form - publicKey
+        emailjs.sendForm('service_ab5m7l2','template_4z5gnn6','#contact-form','KKAJ0Mhc9_izqgKRU')
+        .then(() =>{
+            //show massege add color
+            contactMessage.classList.add('color-blue')
+            contactMessage.textContent = 'Message sent'
+
+            //remove mwssage after 5 s
+            setTimeout(() => {
+                contactMessage.textContent = ''
+                
+            }, 5000);
+        })
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+
+
+
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skill__data', { delay: 200, interval: 70})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
